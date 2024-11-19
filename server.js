f1f2b0a222201a35699951c1b3761f96c9e1179b
Lenora Stevens <lenoraRStevens@gmail.com>
2024-11-18 20:47:11 -0600

    learning activities to add vehicle inventory data and styles

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engin and Templates
@@ -23,10 +25,17 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+
 // Index route
-app.get("/", function(req, res) {
-  res.render("index", { title: "Home"} )
-})
+app.get("/", baseController.buildHome)
+
+// Inventory Route
+app.use("/inv", inventoryRoute)
+
+// app.get("/", function(req, res) {
+//   res.render("index", { title: "Home"} )
+// })
+
 
 /* ***********************
  * Local Server Information
